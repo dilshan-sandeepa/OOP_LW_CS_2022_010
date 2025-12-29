29215523cbca6062647c551c72d144c3d4159b8a
dilshan-sandeepa <dilshansandeepa025@gmail.com>
2025-12-29 18:20:03 +0530

    fdfr

--- src/ss/hfyufgf.java
+++
@@ -1,7 +0,0 @@
-package ss;
-
-public class hfyufgf {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
--- src/ss/ojfd.java
+++
@@ -1,7 +0,0 @@
-package ss;
-
-public class ojfd {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
